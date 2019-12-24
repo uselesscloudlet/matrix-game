50af0568a61d0549c410092f1e771891412f31c8
uselesscloudlet <54158357+uselesscloudlet@users.noreply.github.com>
2019-12-24 19:10:07 +0300

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите размер матрицы (NxM):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(28, 28);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 28;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(594, 230);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Продолжить...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.No;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(16, 340);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(594, 300);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 653);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Матричные игры";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("Вы не ввели значения N и M!", "Ошибка!");
+            }
+            else
+            {
+                int N = Convert.ToInt32(textBox1.Text);
+                int M = Convert.ToInt32(textBox2.Text);
+
+                dataGridView1.RowCount = N;
+                dataGridView1.ColumnCount = M;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int N = Convert.ToInt32(textBox1.Text);
+            int M = Convert.ToInt32(textBox2.Text);
+
+            int[,] matrix = new int[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(dataGridView1[j, i].Value);
+                }
+            }
+
+            if (!SaddlePoint(matrix, N, M))
+            {
+                if (N == M)
+                {
+                    if (N == 2)
+                    {
+                        calculateProbablity(matrix);
+                    }
+                }
+                else if (N == 2 && M != 2)
+                {
+                    drawGraph2xm(matrix, M);
+
+                }
+                else if (M == 2 && N != 2)
+                {
+                    drawGraphnx2(matrix, N);
+                }
+            }
+        }
+
+        private bool SaddlePoint(int[,] matrix, int N, int M)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (isMin(matrix, i, j, M) && isMax(matrix, i, j, N))
+                    {
+                        string sp = "Седловая точка: (" + (i + 1) + ", " + (j + 1) + ")";
+                        string gp = "Цена игры: " + matrix[i, j];
+                        MessageBox.Show(sp + Environment.NewLine + gp, "Седловая точка найдена!");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool isMin(int[,] matrix, int im, int jm, int M)
+        {
+            int min = matrix[im, jm];
+            for (int i = 0; i < M; i++)
+            {
+                if (min > matrix[im, i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }   
+
+        private bool isMax(int[,] matrix, int im, int jm, int N)
+        {
+            int max = matrix[im, jm];
+            for (int i = 0; i < N; i++)
+            {
+                if (max < matrix[i, jm])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void drawGraph2xm(int[,] matrix, int M)
+        {
+            int min = 99999, minr = 99999, sc = 0, ssc = 0, elemIndex = 0;
+            double a1, a2, b1, b2, c1, c2;
+            double x = 0, y = 0;
+            List<double> coords = new List<double>();
+
+            chart1.Series.Clear();
+            
+            for (int i = 0; i < M; i++) // чертим все линии
+            {
+                chart1.Series.Add(Convert.ToString(i));
+                chart1.Series[Convert.ToString(i)].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[Convert.ToString(i)].Color = Color.Red;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                chart1.ChartAreas[0].AxisX.Maximum = 1;
+                chart1.Series[i].Points.AddXY(0, matrix[0, i]);
+                chart1.Series[i].Points.AddXY(1, matrix[1, i]);
+
+                if (min > matrix[0, i])
+                {
+                    min = matrix[0, i];
+                    elemIndex = i;
+                }
+                if (minr > matrix[1, i])
+                {
+                    minr = matrix[1, i];
+                }
+                ssc++;
+            }
+            // y первой точки 0, y второй точки 1
+
+            for (int i = 0; i < M - 1; i++)
+            {
+                a1 = 0 - 1;
+                b1 = matrix[1, i] - matrix[0, i];
+                c1 = matrix[0, i] * 1 - matrix[1, i] * 0;
+                a2 = 0 - 1;
+                b2 = matrix[1, i + 1] - matrix[0, i + 1];
+                c2 = matrix[0, i + 1] * 1 - matrix[1, i + 1] * 0;
+                
+                if ((a1 / a2) == (b1 / b2))
+                {
+                    MessageBox.Show("Линии параллельны");
+                }
+
+                intersect(a1, a2, b1, b2, c1, c2, ref x, ref y);
+                coords.Insert(sc, x);
+                sc++;
+                coords.Insert(sc, y);
+                sc++;
+            }
+            for (int i = 0; i <= coords.Count / 2; i += 2)
+            {
+                MessageBox.Show("x = " + coords[i] + "; y = " + coords[i + 1] + Environment.NewLine);
+            }
+            for (int i = 0; i <= coords.Count / 2; i += 2)
+            {
+                chart1.Series.Add(Convert.ToString(ssc));
+                chart1.Series[Convert.ToString(ssc)].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[Convert.ToString(ssc)].Color = Color.Blue;
+                chart1.Series[ssc].BorderWidth = 3;
+                if (i == 0)
+                {
+                    chart1.Series[ssc].Points.AddXY(0, matrix[0, elemIndex]);
+                    chart1.Series[ssc].Points.AddXY(coords[i + 1], coords[i]);
+                }
+                else
+                {
+                    chart1.Series[ssc].Points.AddXY(coords[i - 1], coords[i - 2]);
+                    chart1.Series[ssc].Points.AddXY(coords[i + 1], coords[i]);
+                }
+                ssc++;
+            }
+            chart1.Series.Add(Convert.ToString(ssc));
+            chart1.Series[Convert.ToString(ssc)].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[Convert.ToString(ssc)].Color = Color.Blue;
+            chart1.Series[ssc].BorderWidth = 3;
+            int fll = coords.Count / 2 + 1;
+            chart1.Series[ssc].Points.AddXY(coords[fll], coords[fll - 1]);
+            chart1.Series[ssc].Points.AddXY(1, minr);
+        }
+
+            private void drawGraphnx2(int[,] matrix, int N)
+        {
+            chart1.Series.Clear();
+
+            for (int i = 0; i < N; i++)
+            {
+                chart1.Series.Add(Convert.ToString(i));
+                chart1.Series[Convert.ToString(i)].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[Convert.ToString(i)].Color = Color.Red;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                chart1.ChartAreas[0].AxisX.Maximum = 1;
+                chart1.Series[i].Points.AddXY(0, matrix[i, 0]);
+                chart1.Series[i].Points.AddXY(1, matrix[i, 1]);
+            }
+        }
+
+        private void calculateProbablity(int[,] matrix)
+        {
+            double v = (matrix[1, 1] * matrix[0, 0] - matrix[0, 1] * matrix[1, 0]) / (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]);
+            double p1 = (matrix[1, 1] - matrix[1, 0]) / (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]);
+            double p2 = (matrix[0, 0] - matrix[0, 1]) / (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]);
+            double q1 = (matrix[1, 1] - matrix[0, 1]) / (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]);
+            double q2 = (matrix[0, 0] - matrix[1, 0]) / (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]);
+
+            if (p1 < 0 || p2 < 0 || q1 < 0 || q2 < 0)
+            {
+                MessageBox.Show("Невозможно вычислить вероятности." + Environment.NewLine +
+                                "Вероятность меньше 0.");
+            }
+            else
+            {
+                string p = "(" + (matrix[1, 1] - matrix[1, 0]) + "/" + (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]) + "," +
+                    (matrix[0, 0] - matrix[0, 1]) + "/" + (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]) + ")";
+                string q = "(" + (matrix[1, 1] - matrix[0, 1]) + "/" + (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]) + ", " +
+                    (matrix[0, 0] - matrix[1, 0]) + "/" + (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]) + ")";
+                string gp = (matrix[1, 1] * matrix[0, 0] - matrix[0, 1] * matrix[1, 0]) + "/" + (matrix[0, 0] + matrix[1, 1] - matrix[0, 1] - matrix[1, 0]) +
+                    " (" + v + ")";
+                MessageBox.Show("P = " + p + Environment.NewLine +
+                                "Q = " + q + Environment.NewLine +
+                                "Цена игры: " + gp);
+            }
+        }
+
+        private void intersect(double a1, double a2, double b1, double b2, double c1, double c2, ref double x, ref double y)
+        {
+            double det = a1 * b2 - a2 * b1;
+            x = (b1 * c2 - b2 * c1) / det;
+            y = (a2 * c1 - a1 * c2) / det;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
